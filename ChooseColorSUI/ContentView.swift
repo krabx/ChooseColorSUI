8fa06522dfc45726a7ebd8a1822e64ee5de3d6d1
Bogdan Radchenko <inroph@mail.ru>
2023-04-23 23:12:08 +0300

    setup project, adding views

--- ChooseColorSUI/ContentView.swift
+++ ChooseColorSUI/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 350, height: 200)
+                .foregroundColor(Color(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue))
+            
+            HStack(alignment: .center,spacing: 20) {
+                VStack(spacing: 20) {
+                    Text("\(redSliderValue)")
+                    Text("\(greenSliderValue)")
+                    Text("\(blueSliderValue)")
+                }
+                
+                VStack() {
+                    Slider(value: $redSliderValue, in: 0...255, step: 1)
+                    Slider(value: $greenSliderValue, in: 0...255, step: 1)
+                    Slider(value: $blueSliderValue, in: 0...255, step: 1)
+                }
+                
+                VStack(spacing: 20) {
+                    TextField("0", text: .constant(""))
+                    TextField("0", text: .constant(""))
+                    TextField("0", text: .constant(""))
+                }
+            }
+            
+            Spacer()
         }
-        .padding()
+
     }
 }
 
