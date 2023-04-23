c9f377d55a9166d379349fd6100268796d09da1e
Bogdan Radchenko <inroph@mail.ru>
2023-04-23 22:52:47 +0300

    Initial Commit

---
+++ ChooseColorSUI/ChooseColorSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseColorSUIApp.swift
+//  ChooseColorSUI
+//
+//  Created by Богдан Радченко on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChooseColorSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
