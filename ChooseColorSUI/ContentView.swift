6cba91a55fccb9b7d6ee094d6c528ff64345a823
Bogdan Radchenko <inroph@mail.ru>
2023-04-24 18:39:11 +0300

    adding value in textfield, adding keyboard type, adding formatter

--- ChooseColorSUI/ContentView.swift
+++ ChooseColorSUI/ContentView.swift
@@ -8,44 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Field {
+        case red
+        case green
+        case blue
+    }
+    
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
     
+    @FocusState private var focusedField: Field?
+    @FocusState private var isFocused: Bool
+    
+    let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        return formatter
+    }()
+    
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 350, height: 200)
-                .foregroundColor(Color(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue))
+        
+        VStack(spacing: 40) {
+            ColorView(redColor: redSliderValue / 255, greenColor: greenSliderValue / 255, blueColor: blueSliderValue / 255)
             
-            HStack(alignment: .center,spacing: 20) {
+            HStack(alignment: .center,spacing: 23) {
                 VStack(spacing: 20) {
-                    Text("\(redSliderValue)")
-                    Text("\(greenSliderValue)")
-                    Text("\(blueSliderValue)")
+                    Text("\(lround(redSliderValue))")
+                    Text("\(lround(greenSliderValue))")
+                    Text("\(lround(blueSliderValue))")
                 }
+                .frame(width: 40)
                 
                 VStack() {
                     Slider(value: $redSliderValue, in: 0...255, step: 1)
+                        .tint(.red)
                     Slider(value: $greenSliderValue, in: 0...255, step: 1)
+                        .tint(.green)
                     Slider(value: $blueSliderValue, in: 0...255, step: 1)
+                        .tint(.blue)
                 }
                 
                 VStack(spacing: 20) {
-                    TextField("0", text: .constant(""))
-                    TextField("0", text: .constant(""))
-                    TextField("0", text: .constant(""))
+                    TextField("Number", value: $redSliderValue, formatter: formatter)
+                        .focused($focusedField, equals: .red)
+                        .keyboardType(.decimalPad)
+                    TextField("0", value: $greenSliderValue, formatter: formatter)
+                        .focused($focusedField, equals: .green)
+                        .keyboardType(.decimalPad)
+                    TextField("0", value: $blueSliderValue, formatter: formatter)
+                        .focused($focusedField, equals: .blue)
+                        .keyboardType(.decimalPad)
                 }
+                .frame(width: 50)
             }
             
             Spacer()
         }
 
+        .padding()
+        .background(.purple)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorView: View {
+    let redColor: Double
+    let greenColor: Double
+    let blueColor: Double
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .frame(width: 350, height: 200)
+            .foregroundColor(Color(red: redColor, green: greenColor, blue: blueColor))
     }
 }
