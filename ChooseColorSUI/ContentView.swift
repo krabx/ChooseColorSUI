949cebb2e715086ebae9ad78f7e3b4945497e756
Bogdan Radchenko <inroph@mail.ru>
2023-04-25 21:54:26 +0300

    final change

--- ChooseColorSUI/ContentView.swift
+++ ChooseColorSUI/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var redTFValue: Double = 0
     @State private var greenTFValue: Double = 0
     @State private var blueTFValue: Double = 0
@@ -16,9 +15,9 @@ struct ContentView: View {
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
-
+    
     @State private var isAlertPresented = false
-
+    
     @FocusState private var isFocused: Bool
     
     let formatter: NumberFormatter = {
@@ -51,44 +50,74 @@ struct ContentView: View {
                 }
                 
                 VStack() {
-                    
                     ColorTextField(textFieldValue: $redTFValue, sliderValue: $redSliderValue, isAlertPresented: $isAlertPresented, isFocused: _isFocused, formatter: formatter)
                     
                     ColorTextField(textFieldValue: $greenTFValue, sliderValue: $greenSliderValue, isAlertPresented: $isAlertPresented, isFocused: _isFocused, formatter: formatter)
                     
                     ColorTextField(textFieldValue: $blueTFValue, sliderValue: $blueSliderValue, isAlertPresented: $isAlertPresented, isFocused: _isFocused, formatter: formatter)
-                    
-//                    TextField("0", value: $greenSliderValue, formatter: formatter)
-//                        .textFieldStyle(.roundedBorder)
-//                        .focused($isFocused)
-//                        .keyboardType(.decimalPad)
-//
-//                    TextField("0", value: $blueSliderValue, formatter: formatter)
-//                        .textFieldStyle(.roundedBorder)
-//                        .focused($isFocused)
-//                        .keyboardType(.decimalPad)
                 }
-//                .toolbar {
-//                    ToolbarItemGroup(placement: .keyboard) {
-//                        Spacer()
-//                        Button("Done") {
-//                            checkNumber(from: redSliderValue)
-//                        }
-//                        .alert("Wrong number", isPresented: $isAlertPresented, actions: { }) {
-//                            Text("Enter correct number (0-255)")
-//                        }
-//                    }
-//                }
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            checkNumber(from: redTFValue) {
+                                redSliderValue = redTFValue
+                            }
+                            checkNumber(from: greenTFValue) {
+                                greenSliderValue = greenTFValue
+                            }
+                            checkNumber(from: blueTFValue) {
+                                blueSliderValue = blueTFValue
+                            }
+                        }
+                        .alert("Wrong number", isPresented: $isAlertPresented, actions: { }) {
+                            Text("Enter correct number (0-255)")
+                        }
+                    }
+                }
                 .frame(width: 50)
             }
-            
             Spacer()
         }
-
+        
         .padding()
         .background(.purple)
         .onTapGesture {
-            isFocused = false
+            checkNumber(from: redTFValue) {
+                redSliderValue = redTFValue
+            }
+            checkNumber(from: greenTFValue) {
+                greenSliderValue = greenTFValue
+            }
+            checkNumber(from: blueTFValue) {
+                blueSliderValue = blueTFValue
+            }
+        }
+    }
+    
+    func checkNumber(from value: Double, completion: () -> Void) {
+        switch value {
+        case redTFValue:
+            if redTFValue > 255 {
+                isAlertPresented.toggle()
+            } else {
+                completion()
+                isFocused = false
+            }
+        case greenTFValue:
+            if greenTFValue > 255 {
+                isAlertPresented.toggle()
+            } else {
+                completion()
+                isFocused = false
+            }
+        default:
+            if blueTFValue > 255 {
+                isAlertPresented.toggle()
+            } else {
+                completion()
+                isFocused = false
+            }
         }
     }
 }
@@ -122,37 +151,15 @@ struct ColorTextField: View {
     let formatter: Formatter
     
     var body: some View {
-        
         TextField("0", value: $textFieldValue, formatter: formatter)
             .textFieldStyle(.roundedBorder)
             .focused($isFocused)
             .keyboardType(.decimalPad)
-            .toolbar {
-                ToolbarItemGroup(placement: .keyboard) {
-                    Spacer()
-                    Button("Done") {
-                        checkNumber(from: textFieldValue)
-                    }
-                    .alert("Wrong number", isPresented: $isAlertPresented, actions: {} ) {
-                        Text("Enter correct number")
-                    }
-
-                }
-            }
             .onAppear {
                 textFieldValue = sliderValue
             }
             .onChange(of: sliderValue) { _ in
                 textFieldValue = sliderValue
             }
-    }
-    
-    private func checkNumber(from value: Double) {
-        if value < 0 || value > 255 {
-            isAlertPresented.toggle()
-        } else {
-            sliderValue = value
-            isFocused = false
-        }
     }
 }
